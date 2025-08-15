d20216ea8e1e79800a600d37233cdb264ffe0c43
Ratnesh <ratneshbvk2504@gmail.com>
2025-08-15 23:06:32 +0530

    Adding BlogCard to Home page for reusability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { FaArrowRight, FaBlog, FaUserShield } from "react-icons/fa";
+import { FaBlog, FaUserShield } from "react-icons/fa";
+import BlogCard from "../components/BlogCard";
 
 export default function Home() {
   const posts = useSelector((state) => state.posts.posts);
@@ -43,28 +44,7 @@ export default function Home() {
 
         <div className="grid gap-8 md:grid-cols-3">
           {posts.slice(0, 3).map((post) => (
-            <div
-              key={post.id}
-              className="bg-white shadow-sm rounded-lg overflow-hidden hover:shadow-md transition"
-            >
-              <img
-                src={post.image}
-                alt={post.title}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-5">
-                <h3 className="text-lg font-semibold text-gray-900">
-                  {post.title}
-                </h3>
-                <p className="text-sm text-gray-600 mt-2">{post.excerpt}</p>
-                <Link
-                  to={`/blog/${post.slug}`}
-                  className="inline-flex items-center gap-1 text-blue-600 mt-4 text-sm hover:underline"
-                >
-                  Read More <FaArrowRight size={12} />
-                </Link>
-              </div>
-            </div>
+            <BlogCard key={post.id} post={post} />
           ))}
         </div>
       </section>
